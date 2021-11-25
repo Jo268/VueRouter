47f38923fd68ee50a17d4fe63dcf31bf8139a248
Jo Piene <joluca@gmx.net>
2021-11-25 15:41:49 +0100

    A view Router project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
+  </div>
+  <div class="container">
+  <router-view></router-view>
+  </div>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.container{
+  border: 2px gray;
+}
+</style>
---
+++ src/views/Brazil.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="Brazil">
+    <h1>Brazil</h1>
+  </div>
+</template>
+
